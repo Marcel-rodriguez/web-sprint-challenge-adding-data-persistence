32b7e722ac1fac7d53fc1b761e3d42fab8f0884e
marcel <marcel_rodrgez@yahoo.com>
2022-02-18 08:22:32 -0500

    scafolded project added deps created server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
 // build your server here and require it from index.js
+const express = require('express')
+const morgan = require('morgan')
+
+const tasksRouter = require('./recipes/tasks-router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/recipes', tasksRouter)
+
+server.use('*', (req, res) => {
+    res.json({api: 'up'})
+})
+
+module.exports = server;
