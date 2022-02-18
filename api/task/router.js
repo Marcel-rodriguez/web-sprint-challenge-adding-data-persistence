533f1c232181406b7cef913beb7cb8dea3d0055f
marcel <marcel_rodrgez@yahoo.com>
2022-02-18 11:35:27 -0500

    Made initial get and posts inside of each router, made an error handler inside each router

--- api/task/router.js
+++ api/task/router.js
@@ -2,4 +2,20 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+
+})
+
+router.post('/', (req, res, next) => {
+    
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        custom: 'something went wrong inside the tasks router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
 module.exports = router
