79460fe184c26cd8354b63f82eba305d007b75db
marcel <marcel_rodrgez@yahoo.com>
2022-02-18 10:34:19 -0500

    got server working, created routers, added more dd

--- api/server.js
+++ api/server.js
@@ -2,13 +2,18 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const tasksRouter = require('./recipes/tasks-router')
+const tasksRouter = require('./task/router')
+const resourceRouter = require('./resource/router')
+const projectRouter = require('./project/router')
 
 const server = express()
 
 server.use(express.json())
+server.use(morgan('tiny'))
 
 server.use('/api/recipes', tasksRouter)
+server.use('/api/resources', resourceRouter)
+server.use('/api/projects', projectRouter)
 
 server.use('*', (req, res) => {
     res.json({api: 'up'})
